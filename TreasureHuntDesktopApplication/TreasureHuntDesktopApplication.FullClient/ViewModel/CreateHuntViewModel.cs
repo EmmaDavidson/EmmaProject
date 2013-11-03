a517c0b80d6f2492820fb4677ddc13319af1c83c
EmmaDavidson <edavidson11@qub.ac.uk>
2013-11-03 17:21:24 +0000

    Initial start to the project  - Treasure Hunt Desktop Application
    I have begun to implement my FYP by adding a WPF application.

---
+++ TreasureHuntDesktopApplication/TreasureHuntDesktopApplication.FullClient/ViewModel/CreateHuntViewModel.cs
@@ -0,0 +1,113 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TreasureHuntDesktopApplication.FullClient.ViewModel
+{
+    class CreateHuntViewModel : ViewModelBase
+    {
+        //Create initiation of the service here
+
+        public RelayCommand SaveQuestionCommand { get; set; }
+        public RelayCommand UpdateQuestionCommand { get; set; }
+
+        public CreateHuntViewModel()
+        {
+            //set up the service
+            //set up the relay command
+            SaveQuestionCommand = new RelayCommand(() => ExecuteSaveQuestionCommand, ValidQuestion());
+            UpdateQuestionCommand = new RelayCommand(()=> ExecuteUpdateQuestionCommand, ValidQuestion());
+            RefreshTreasureHunts();
+        
+        }
+
+        private void RefreshTreasureHunts()
+        {
+            //Get the treasure hunts that have been saved
+            //this.serviceClient.GetTreasureHuntsAsync();
+            //this.questions = this.serviceClient.GetTreasureHunts();
+        }
+
+        private void RefreshQuestions()
+        {
+            //Get the questions 
+            //this.serviceClient.GetQuestionsAsync(this.CurrentTreasureHunt.TreasureHuntId);
+            //this.Questions = this.serviceClient.GetQuestions(this.CurrentTreasureHunt.TreasureHuntId);
+
+        }
+
+        private IEnumerable<Question> questions;
+        public IEnumerable<Question> Questions
+        {
+
+            get { return this.questions; }
+            set {
+                
+                this.questions = value;
+                RaisePropertyChanged("Questions");
+            }
+        
+        }
+
+        private IEnumerable<TreasureHunt> treasureHunts;
+        public IEnumerable<TreasureHunt> TreasureHunts;
+        {
+
+            get { return this.treasureHunts; }
+            set {
+                
+                this.treasureHunts = value;
+                RaisePropertyChanged("TreasureHunts");
+            }
+        
+        }
+
+        private TreasureHunt currentTreasureHunt;
+        public TreasureHunt CurrentTreasureHunt;
+        {
+
+            get { return this.currentTreasureHunt; }
+            set {
+                
+                this.currentTreasureHunt = value;
+                RaisePropertyChanged("CurrentTreasureHunt");
+                RefreshQuestions();
+
+            }
+        
+        }
+
+
+        private Question currentQuestion;
+        public Question CurrentQuestion
+        {
+
+            get { return this.currentQuestion; }
+            set
+            {
+
+                this.currentQuestion = value;
+                RaisePropertyChanged("CurrentQuestion");
+            }
+
+        
+
+        private void ExecuteSaveQuestionCommand()
+        {
+            //Create a new instance of the question and add the appropriate variables
+            //call the service here to save the question to the database
+            //this.serviceClient.saveQuestion(Question);
+        }
+
+        private void ExecuteUpdateQuestionCommand()
+        {
+            //this.serviceClient.SaveQuestionAsync(this.CurrentQuestion);
+
+        }
+
+
+    }
+}
---
+++ TreasureHuntDesktopApplication/TreasureHuntDesktopApplication.FullClient/ViewModel/MainViewModel.cs
@@ -0,0 +1,55 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.ComponentModel;
+using System.Windows.Input;
+using TreasureHuntDesktopApplication.FullClient.Model;
+
+namespace TreasureHuntDesktopApplication.FullClient.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase, IMainViewModel
+    {
+        //-http://www.codeproject.com/Articles/72724/Beginning-a-WPF-MVVM-application-Navigating-betwee
+
+        private ViewModelBase currentViewModel;
+        public RelayCommand NavigateToCreateHuntViewCommand;
+        private CreateHuntViewModel newViewModel = new CreateHuntViewModel();
+
+        public MainViewModel()
+        {
+            NavigateToCreateHuntViewCommand = new RelayCommand(() => NavigateToView(newViewModel));
+        }
+
+        /// <summary>
+        /// Gets the current view model that is being shown
+        /// </summary>
+        public ViewModelBase CurrentViewModel
+        {
+            get
+            {
+                return currentViewModel;
+            }
+            set
+            {
+                if (currentViewModel == value)
+                    return;
+                currentViewModel = value;
+                RaisePropertyChanged("CurrentViewModel");
+            }
+        }
+
+
+
+     
+
+        public void NavigateToView(ViewModelBase viewToNavigate)
+        {
+            CurrentViewModel = viewToNavigate;
+        }
+    }
+}
