edbc51a106ac28bd07771230134cacf0e336514a
EmmaDavidson <edavidson11@qub.ac.uk>
2013-11-25 23:04:22 +0000

    Sprint 3 Commit
    Mobile Application created. Attempting to get JSON/PHP talking to
    another another with the mobile application.

---
+++ TreasureHuntMobileApplication/TreasureHunt/src/com/application/treasurehunt/LoginActivity.java
@@ -0,0 +1,299 @@
+package com.application.treasurehunt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import Utilities.JSONParser;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ *FILE GENERATED AUTOMATICALLY WHEN CREATING NEW LOGIN ACTIVITY
+ */
+public class LoginActivity extends Activity {
+	//http://www.mybringback.com/tutorial-series/13193/android-mysql-php-json-part-5-developing-the-android-application/
+	JSONParser jsonParser = new JSONParser();
+	private static final String myLoginUrl =  "http://127.0.0.1:80/webservice/login.php";
+	private static final String tagSuccess = "success";
+	private static final String tagMessage = "message";
+	
+	
+	/**
+	 * Keep track of the login task to ensure we can cancel it if requested.
+	 */
+	private UserLoginTask mAuthTask = null;
+	private ProgressDialog pDialog;
+
+	// Values for email and password at the time of the login attempt.
+	private String mEmail;
+	private String mPassword;
+
+	// UI references.
+	private EditText mEmailView;
+	private EditText mPasswordView;
+	private View mLoginFormView;
+	private View mLoginStatusView;
+	private TextView mLoginStatusMessageView;
+	
+	public static final String EXTRA_EMAIL = "com.example.android.authenticatordemo.extra.EMAIL";
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_login);
+
+		//AUTO GENERATED
+		//mEmail = getIntent().getStringExtra(EXTRA_EMAIL);
+		mEmailView = (EditText) findViewById(R.id.email);
+		//mEmailView.setText(mEmail);
+
+		mPasswordView = (EditText) findViewById(R.id.password);
+		
+		/*mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+					@Override
+					public boolean onEditorAction(TextView textView, int id,
+							KeyEvent keyEvent) {
+						if (id == R.id.login || id == EditorInfo.IME_NULL) {
+							attemptLogin();
+							return true;
+						}
+						return false;
+					}
+				}); */
+
+		mLoginFormView = findViewById(R.id.login_form);
+		mLoginStatusView = findViewById(R.id.login_status);
+		mLoginStatusMessageView = (TextView) findViewById(R.id.login_status_message);
+
+		findViewById(R.id.sign_in_button).setOnClickListener(
+				new View.OnClickListener() {
+					@Override
+					public void onClick(View view) {
+						attemptLogin();
+					}
+				});
+		
+		findViewById(R.id.register_on_login_button).setOnClickListener(
+				new View.OnClickListener() {
+					@Override
+					public void onClick(View view) {
+						goToRegister();
+					}
+				});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		getMenuInflater().inflate(R.menu.login, menu);
+		return true;
+	}
+
+	public void goToRegister()
+	{
+		Intent intent = new Intent(this, RegisterActivity.class);
+		startActivity(intent);
+	}
+	
+	/**
+	 * Attempts to sign in or register the account specified by the login form.
+	 * If there are form errors (invalid email, missing fields, etc.), the
+	 * errors are presented and no actual login attempt is made.
+	 */
+	public void attemptLogin() {
+		if (mAuthTask != null) {
+			return;
+		}
+
+		// Reset errors.
+		mEmailView.setError(null);
+		mPasswordView.setError(null);
+
+		// Store values at the time of the login attempt.
+		mEmail = mEmailView.getText().toString();
+		mPassword = mPasswordView.getText().toString();
+
+		boolean cancel = false;
+		View focusView = null;
+
+		// Check for a valid password.
+		if (TextUtils.isEmpty(mPassword)) {
+			mPasswordView.setError(getString(R.string.error_field_required));
+			focusView = mPasswordView;
+			cancel = true;
+		} else if (mPassword.length() < 4) {
+			mPasswordView.setError(getString(R.string.error_invalid_password));
+			focusView = mPasswordView;
+			cancel = true;
+		}
+
+		// Check for a valid email address.
+		if (TextUtils.isEmpty(mEmail)) {
+			mEmailView.setError(getString(R.string.error_field_required));
+			focusView = mEmailView;
+			cancel = true;
+		} else if (!mEmail.contains("@")) {
+			mEmailView.setError(getString(R.string.error_invalid_email));
+			focusView = mEmailView;
+			cancel = true;
+		}
+
+		if (cancel) {
+			// There was an error; don't attempt login and focus the first
+			// form field with an error.
+			focusView.requestFocus();
+		} else {
+			// Show a progress spinner, and kick off a background task to
+			// perform the user login attempt.
+			mLoginStatusMessageView.setText(R.string.login_progress_signing_in);
+			showProgress(true);
+			mAuthTask = new UserLoginTask();
+			mAuthTask.execute((String) null);
+		}
+	}
+
+	/**
+	 * Shows the progress UI and hides the login form.
+	 */
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+	private void showProgress(final boolean show) {
+		// On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+		// for very easy animations. If available, use these APIs to fade-in
+		// the progress spinner.
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+			int shortAnimTime = getResources().getInteger(
+					android.R.integer.config_shortAnimTime);
+
+			mLoginStatusView.setVisibility(View.VISIBLE);
+			mLoginStatusView.animate().setDuration(shortAnimTime)
+					.alpha(show ? 1 : 0)
+					.setListener(new AnimatorListenerAdapter() {
+						@Override
+						public void onAnimationEnd(Animator animation) {
+							mLoginStatusView.setVisibility(show ? View.VISIBLE
+									: View.GONE);
+						}
+					});
+
+			mLoginFormView.setVisibility(View.VISIBLE);
+			mLoginFormView.animate().setDuration(shortAnimTime)
+					.alpha(show ? 0 : 1)
+					.setListener(new AnimatorListenerAdapter() {
+						@Override
+						public void onAnimationEnd(Animator animation) {
+							mLoginFormView.setVisibility(show ? View.GONE
+									: View.VISIBLE);
+						}
+					});
+		} else {
+			// The ViewPropertyAnimator APIs are not available, so simply show
+			// and hide the relevant UI components.
+			mLoginStatusView.setVisibility(show ? View.VISIBLE : View.GONE);
+			mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+		}
+	}
+
+	/**
+	 * Represents an asynchronous login/registration task used to authenticate
+	 * the user.
+	 */
+	public class UserLoginTask extends AsyncTask<String, String, String> {
+		
+		@Override
+		protected void onPreExecute()
+		{
+			super.onPreExecute();
+			pDialog = new ProgressDialog(LoginActivity.this);
+            pDialog.setMessage("Attempting login...");
+			pDialog.setIndeterminate(false);
+			pDialog.setCancelable(true);
+			pDialog.show();
+		}
+		
+		@Override
+		protected String doInBackground(String... args) {
+//http://www.mybringback.com/tutorial-series/13193/android-mysql-php-json-part-5-developing-the-android-application/
+			
+			int success;
+			String email = mEmail;
+			String password = mPassword;
+			
+			try {
+				List<NameValuePair> parameters = new ArrayList<NameValuePair>();
+				
+				parameters.add(new BasicNameValuePair("Email", email));
+				parameters.add(new BasicNameValuePair("Password", password));
+				
+				Log.d("request", "starting");
+				JSONObject json = jsonParser.makeHttpRequest(myLoginUrl, "POST", parameters);
+				Log.d("Login attempt", json.toString());
+				
+				success = json.getInt(tagSuccess);
+				if(success == 1)
+				{
+					Log.d("Login Successful!", json.toString());
+					Intent scanQRCodeActivityIntent = new Intent(LoginActivity.this, ScanQRCodeActivity.class);
+					finish();
+					startActivity(scanQRCodeActivityIntent);
+					return json.getString(tagMessage);
+				}
+				else
+				{
+					Log.d("Login failed!", json.getString(tagMessage));
+					return json.getString(tagMessage);
+				}
+				
+			} catch (JSONException e) {
+			
+			}
+
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(final String fileUrl) {
+			mAuthTask = null;
+			showProgress(false);
+			pDialog.dismiss();
+
+			if (fileUrl != null) {
+				finish();
+				Toast.makeText(LoginActivity.this, fileUrl, Toast.LENGTH_LONG);
+			} else {
+				/*mPasswordView
+						.setError(getString(R.string.error_incorrect_password));
+				mPasswordView.requestFocus(); */
+				Toast.makeText(LoginActivity.this, "Invalid details", Toast.LENGTH_LONG).show();
+			}
+		}
+
+		@Override
+		protected void onCancelled() {
+			mAuthTask = null;
+			showProgress(false);
+		}
+	}
+}
---
+++ TreasureHuntMobileApplication/TreasureHunt/src/com/application/treasurehunt/ScanQRCodeActivity.java
@@ -0,0 +1,55 @@
+package com.application.treasurehunt;
+
+import com.dm.zbar.android.scanner.ZBarConstants;
+import com.dm.zbar.android.scanner.ZBarScannerActivity;
+//import com.google.zxing.integration.android.IntentIntegrator;
+//import com.google.zxing.integration.android.IntentResult;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+//http://mobile.tutsplus.com/tutorials/android/android-sdk-create-a-barcode-reader/ - ZXING
+//https://github.com/DushyanthMaguluru/ZBarScanner for ZBAR - USING THIS CURRENTLY, referencing the source forge project
+//Whole Activity taken from this either of these websites
+
+public class ScanQRCodeActivity extends Activity implements OnClickListener {
+
+	private Button scanButton;
+	private TextView contentText;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_scan_qrcode);
+		
+		scanButton = (Button)findViewById(R.id.scan_qr_code_button);
+		contentText = (TextView)findViewById(R.id.scan_content_received);
+		scanButton.setOnClickListener(this);
+	}
+	
+	//For now - deciding to use ZBar within application so user does not have to have another application running to scan
+	@Override
+	public void onClick(View v) {	
+		Intent intent = new Intent(this, ZBarScannerActivity.class);
+		startActivityForResult(intent, 1);
+	}
+	
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) 
+	{
+		if (resultCode == RESULT_OK) 
+	    {	
+			contentText.setText("QUESTION: " + intent.getStringExtra(ZBarConstants.SCAN_RESULT));
+
+	    } else if(resultCode == RESULT_CANCELED) {
+	        Toast.makeText(this, "Camera unavailable", Toast.LENGTH_SHORT).show();
+	    }
+	}
+
+}
